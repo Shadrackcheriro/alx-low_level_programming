47c19196c6a3cb74e42fb500ac244334492aa240
Shadrack Cheriro <shadrackcheriro65@gmail.com>
2023-06-15 15:19:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - c program with UNIX
- * Return: 0
+ * Return: 1
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
