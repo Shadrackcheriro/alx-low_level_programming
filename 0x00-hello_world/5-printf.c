70af96a1f3071ac5e8b264fcdb3f5797487e418c
Shadrack Cheriro <shadrackcheriro65@gmail.com>
2023-06-15 09:07:36 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Printing a programm using printf
+ * Return: 0 (passed)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
