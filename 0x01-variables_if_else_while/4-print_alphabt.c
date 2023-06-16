3a2e1e81a7f7057d84be4369e769e5a2bae18f3d
Shadrack Cheriro <shadrackcheriro65@gmail.com>
2023-06-16 09:09:01 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry code
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z')
+		{
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++
+		}
+	putchar('\n');
+	return (0);
+}
