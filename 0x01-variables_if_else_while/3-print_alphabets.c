7589c1851b776cd40729379a1c38e4eb721f7fcd
Shadrack Cheriro <shadrackcheriro65@gmail.com>
2023-06-16 08:43:42 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z')
+		{
+			putchur(c);
+			c++;
+		}
+	while
+		(d <= 'z')
+		{
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
