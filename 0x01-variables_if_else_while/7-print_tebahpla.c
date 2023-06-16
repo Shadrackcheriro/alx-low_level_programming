efaa326a6a301e0b74d2c818313def14beb6cf45
Shadrack Cheriro <shadrackcheriro65@gmail.com>
2023-06-16 10:20:10 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a')
+		{
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
