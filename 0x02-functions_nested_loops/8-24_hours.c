38f753fc133b97c494b51e8a0e9081f0121e1c25
Shadrack Cheriro <shadrackcheriro65@gmail.com>
2023-06-20 11:43:37 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
  * jack_bauer - Prints every minute of the day of Jack Bauer
@@ -22,13 +22,13 @@ void jack_bauer(void)
 					{
 						_putchar(a + '0');
 						_putchar(b + '0');
-						_putchar(50);
+						_putchar(58);
 						_putchar(c + '0');
 						_putchar(d + '0');
 						_putchar('\n');
-					}
-				}
-			}
-		}
-	}
+}
+}
+}
+}
+}
 }
