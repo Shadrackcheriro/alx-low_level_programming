e5331c4d469209ec721cf4c4c0cb1c7e5800d162
Shadrack Cheriro <shadrackcheriro65@gmail.com>
2023-06-20 02:26:05 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,15 @@
 void print_alphabet_x10(void)
 {
 	char c;
-	int i;
+	int i = 0;
 
 	while (i <= 9)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
-
-			_putchar('\n');
-			i++;
 		}
+		_putchar('\n');
+		i++;
 	}
 }
