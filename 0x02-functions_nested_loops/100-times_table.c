f019f656ce79fd5b041d3131ed204f9477b859e5
Shadrack Cheriro <shadrackcheriro65@gmail.com>
2023-06-21 03:39:04 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,7 +24,10 @@ void print_times_table(int n)
 				prod = num * mult;
 				if (prod <= 99)
 					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
 				if (prod >= 100)
+				{
 					_putchar((prod / 100) + '0');
 				_putchar(((prod / 10)) % 10 + '0');
 			}
@@ -34,7 +37,7 @@ void print_times_table(int n)
 			}
 			_putchar((prod % 10) + '0');
 		}
-		_putchar(''\n');
+		_putchar('\n');
 	}
 }
 }
