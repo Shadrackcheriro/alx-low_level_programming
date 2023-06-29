d26a10c337a7f0ed4e211df6fa5ca222d109352b
Shadrack Cheriro <shadrackcheriro65@gmail.com>
2023-06-29 07:52:22 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,16 +8,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int length of string, z;
+	int length_of_string, z;
 
 	length_of_string = 0;
-	while (dest(length of string) != '\0')
+	while (dest[length_of_string] != '\0')
 	{
 		length_of_string++;
 	}
-	for (z = 0; src(z) != '\0'; z++, length_of_string++)
+	for (z = 0; src[z] != '\0'; z++, length_of_string++)
 	{
-		dest[length_of_string] = src[j];
+		dest[length_of_string] = src[z];
 	}
 	dest[length_of_string] = '\0';
 	return (dest);
