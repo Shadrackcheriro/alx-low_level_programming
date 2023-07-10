656c7368f7708a1b17e916c775700b8bfbf16965
Shadrack Cheriro <shadrackcheriro65@gmail.com>
2023-07-10 08:44:15 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,23 +1,49 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * main - checks the code for ALX School students.
+ * *str_concat - concatenates two strings
+ * @s1: string to concatenate
+ * @s2: other string to concatenate
  *
- * Return: Always 0.
+ * Return: pointer to the new string created (Success), or NULL (Error)
  */
-int main(void)
+char *str_concat(char *s1, char *s2)
 {
-	char *s;
+        char *s3;
+        unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	s = str_concat("Betty ", "Holberton");
-	if (s == NULL)
-	{
-		printf("failed\n");
-		return (1);
-	}
-	printf("%s\n", s);
-	free(s);
-		return (0);
+        while (s1 && s1[len1])
+                len1++;
+        while (s2 && s2[len2])
+                len2++;
+
+        s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+        if (s3 == NULL)
+                return (NULL);
+
+        i = 0;
+        j = 0;
+
+        if (s1)
+        {
+                while (i < len1)
+                {
+                        s3[i] = s1[i];
+                        i++;
+                }
+        }
+
+        if (s2)
+        {
+                while (i < (len1 + len2))
+                {
+                        s3[i] = s2[j];
+                        i++;
+                        j++;
+                }
+        }
+        s3[i] = '\0';
+
+return (s3);
 }
